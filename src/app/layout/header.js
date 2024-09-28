7c18abb94187ac7035602e765a8463e8f4670bd7
Jojo <joebert_italia09@yahoo.com>
2024-09-29 01:35:26 +0800

    add routing pages:

---
+++ src/app/layout/header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/students">Students</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
---
+++ src/app/students/page.js
@@ -0,0 +1,10 @@
+import Header from "../layout/header";
+
+export default function Students() {
+  return (
+    <>
+      <Header />
+      This is students page
+    </>
+  )
+}
